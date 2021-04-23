5e794a67efa4757398179499a8282d9186752b77
minthing <minjeongandk@gmail.com>
2021-04-24 00:24:41 +0900

    무한 스크롤 구현

--- ch1/front/store/posts.js
+++ ch1/front/store/posts.js
@@ -2,7 +2,11 @@
 
 export const state = () => ({
     mainPosts :[],
+    hasMorePost:true, //인피니트 스크롤링을 위한 준비
   })
+
+  const totalPosts = 51;
+  const limit = 10;
   
   export const mutations = { // 일반 객체로 만들어야 함
     addMainPosts(state, payload) {
@@ -15,6 +19,21 @@ export const state = () => ({
     addComment(state, payload){
       const index = state.mainPosts.findIndex(v => v.id === payload.postId);
       state.mainPosts[index].Comments.unshift(payload);
+    },
+    loadPosts(state){
+      const diff = totalPosts - state.mainPosts.length; // 아직 안 불러온 게시글 수 (계속 바뀜)
+      const fakePosts = Array(diff > limit ? limit:diff).fill().map(v => ({
+        id:Math.random().toString(),
+        User:{
+          id:1,
+          nickname:'minthing'
+        },
+        content:`${Math.random().toString()}번째 게시물 입니다.`,
+        Comments:[],
+        Images:[],
+      }));
+      state.mainPosts = state.mainPosts.concat(fakePosts);
+      state.hasMorePost = fakePosts.length === limit; // 10의 배수가 아니면 남은 포스트 갯수가 10 이하.. 이므로 마지막 페이지임
     }
   }
 
@@ -29,5 +48,10 @@ export const state = () => ({
 
     addComment({commit}, payload){
       commit('addComment', payload);
+    },
+    loadPosts({commit, state}, payload){
+      if(state.hasMorePost){ // 쓸데없는 요청 막기
+        commit('loadPosts', payload);
+      }
     }
   }
