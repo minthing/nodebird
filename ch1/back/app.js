7ff23f1848add959ef6fe19e97b0e473d067eecf
minthing <minjeongandk@gmail.com>
2021-04-27 20:31:39 +0900

    백엔드 기초 지식 관련 학습

--- ch1/back/app.js
+++ ch1/back/app.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello world');
+app.get('/', (req, res) => { // http GET 멸령
+    res.send('hello world'); //응답이 200일 경우 body에 들어감
 })
 
-app.listen(3085, ()=>{
+app.listen(3085, ()=>{ 
     console.log(`welcome to backend port number ${3085}`);
 })
