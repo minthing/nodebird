8e7efce9e156f4b504aa438f929d86426f6e1b1b
minthing <minjeongandk@gmail.com>
2021-05-11 21:41:00 +0900

    router 연결

--- ch1/back/app.js
+++ ch1/back/app.js
@@ -1,123 +1,45 @@
-
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
 const passport = require('passport');
 const session = require('express-session');
-const morgan = require('morgan');
 const cookie = require('cookie-parser');
+const morgan = require('morgan');
 
 const db = require('./models');
 const passportConfig = require('./passport');
+const userRouter = require('./routes/user');
+const app = express();
 
-//db.sequelize.sync({force:true});
 db.sequelize.sync();
 passportConfig();
-const app = express();
 
 app.use(morgan('dev'));
 app.use(cors({
-    origin : 'http://localhost:3000',
-    credentials : true,
+  origin: 'http://localhost:3000',
+  credentials: true,
 }));
+app.use('/', express.static('uploads'));
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookie('cookiesecret'));
 app.use(session({
-    resave : false,
-    saveUninitialized : false,
-    secret : 'cookiesecret',
-    cookie : {
-        httpOnly : true,
-        sercure : false,
-    }
+  resave: false,
+  saveUninitialized: false,
+  secret: 'cookiesecret',
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', (req, res)=> {
-    res.send('안녕 백엔드');
+app.get('/', (req, res) => {
+  res.status(200).send('안녕 제로초');
 });
 
-app.post('/user', async (req, res, next) => {
-    try{
-        const hash = await bcrypt.hash(req.body.password, 12);
-        const exUser = await db.User.findOne({
-            where:{
-                email:req.body.email,
-            }
-        });
-
-        if(exUser) { //이미 회원가입 되어있으면
-            return res.status(403).json({
-                errorCode: 1,
-                message: '이미 회원가입 되어있습니다.'
-            })
-        };
-        
-        const newUser = await db.User.create({
-            //where:{
-                nickname:req.body.nickname,
-                password:hash,
-                email:req.body.email,
-           // }
-        });
-
-        passport.authenticate('local', (err, user, info) => {
-            if(err) {
-                console.error(err);
-                return next(err);
-            }
-    
-            if(info){
-                return res.status(401).send(info.reason);
-            }
-    
-            return req.login(user, (err) => {
-                if(err){
-                    console.error(err);
-                    return next(err);
-                }
-    
-                return res.json(user);
-            });
-    
-        })(req, res, next);
-
-    } catch (err) {
-        console.log(err);
-        next(err);
-    }
-   
-});
-
-const user = {
-
-}
-
-app.post('/user/login', (req, res, next) => {
-    passport.authenticate('local', (err, user, info) => {
-        if(err) {
-            console.error(err);
-            return next(err);
-        }
-
-        if(info){
-            return res.status(401).send(info.reason);
-        }
-
-        return req.login(user, (err) => {
-            if(err){
-                console.error(err);
-                return next(err);
-            }
-
-            return res.json(user);
-        });
-
-    })(req, res, next);
-});
+app.use('/user', userRouter);
 
 app.listen(3085, () => {
-    console.log(`백엔드 서버 ${3085}번 포트에서 작동중.`);
-}) 
+  console.log(`백엔드 서버 ${3085}번 포트에서 작동중.`);
+});
--- ch1/front/store/user.js
+++ ch1/front/store/user.js
@@ -1,126 +1,122 @@
 export const state = () => ({
-    me :null,
-    followingList:[{
-      id:1, nickname:'one'
-    },{
-      id:2, nickname:'two'
-    },{
-      id:3, nickname: 'three'
-    }],
-    followerList:[{
-      id:1, nickname:'one'
-    },{
-      id:2, nickname:'two'
-    },{
-      id:3, nickname: 'three'
-    }],
-    hasMoreFollower:true,
-    hasMoreFollowing:true
-  })
+  me: null,
+  followerList: [],
+  followingList: [],
+  hasMoreFollower: true,
+  hasMoreFollowing: true,
+});
 
-  const totalFollowers = 8;
-  const totalFollowings = 6;
-  const limit = 3;
+const totalFollowers = 8;
+const totalFollowings = 6;
+const limit = 3;
 
-  
-  export const mutations = { // 일반 객체로 만들어야 함
-    setMe(state, payload) { // payload : state를 바꿀 수 있게 해줌
+export const mutations = {
+  setMe(state, payload) {
+    state.me = payload;
+  },
+  changeNickname(state, payload) {
+    state.me.nickname = payload.nickname;
+  },
+  addFollower(state, payload) {
+    state.followerList.push(payload);
+  },
+  addFollowing(state, payload) {
+    state.followingList.push(payload);
+  },
+  removeFollower(state, payload) {
+    const index = state.followerList.findIndex(v => v.id === payload.id);
+    state.followerList.splice(index, 1);
+  },
+  removeFollowing(state, payload) {
+    const index = state.followingList.findIndex(v => v.id === payload.id);
+    state.followingList.splice(index, 1);
+  },
+  loadFollowings(state) {
+    const diff = totalFollowings - state.followingList.length;
+    const fakeUsers = Array(diff > limit ? limit : diff).fill().map(v => ({
+      id: Math.random().toString(),
+      nickname: Math.floor(Math.random() * 1000),
+    }));
+    state.followingList = state.followingList.concat(fakeUsers);
+    state.hasMoreFollowing = fakeUsers.length === limit;
+  },
+  loadFollowers(state) {
+    const diff = totalFollowers - state.followerList.length;
+    const fakeUsers = Array(diff > limit ? limit : diff).fill().map(v => ({
+      id: Math.random().toString(),
+      nickname: Math.floor(Math.random() * 1000),
+    }));
+    state.followerList = state.followerList.concat(fakeUsers);
+    state.hasMoreFollower = fakeUsers.length === limit;
+  },
+};
 
-      state.me = payload; // 비동기 작업이 있으면 안됨! ajax 요청도 안됨!
-    },
-    changeNickname(state, payload){
-      state.me.nickname = payload.nickname
-    },
-    addFollowing(state, payload){
-      state.following.push(payload)
-    },
-    addFollower(state, payload){
-      state.followerList.push(payload)
-    },
-    removeFollowing(state, payload){ 
-      const index = state.followingList.findIndex(v => v.id === payload.id);
-      state.followingList.splice(index, 1)
-    },
-    removeFollower(state, payload){
-      const index = state.followerList.findIndex(v => v.id === payload.id);
-      state.followerList.splice(index, 1)
-    },
-    loadFollowings(state) {
-      const diff = totalFollowings - state.followingList.length;
-      const fakeUsers = Array(diff > limit ? limit : diff).fill().map(v => ({
-        id: Math.random().toString(),
-        nickname: Math.floor(Math.random() * 1000),
-      }));
-      state.followingList = state.followingList.concat(fakeUsers);
-      state.hasMoreFollowing = fakeUsers.length === limit;
-    },
-    loadFollowers(state) {
-      const diff = totalFollowers - state.followerList.length;
-      const fakeUsers = Array(diff > limit ? limit : diff).fill().map(v => ({
-        id: Math.random().toString(),
-        nickname: Math.floor(Math.random() * 1000),
-      }));
-      state.followerList = state.followerList.concat(fakeUsers);
-      state.hasMoreFollower = fakeUsers.length === limit;
-    },
-  };
-
-export const actions = { //비동기적 작업을 위해 actions를 사용함
-    // context object 안에 있는 것들
-    // commit : mutation 함수를 실행
-    // dispatch : actions를 실행
-    signUp({commit, state}, payload){
-        // 서버에 회원가입 요청을 보냄 payload에 회원정보 들어감
-        console.log(this.$axios);
-        this.$axios.post('http://localhost:3085/user', {
-          email:payload.email, nickname:payload.nickname, password:payload.password
-        }).then((data) => { // promise
-          console.log(data);
-        }).catch((error)=>{
-          console.error(error);
-        }); //rest api
-        commit('setMe', payload);
-    },
-    logIn({commit}, payload){
-      console.log(this.$axios);
-      this.$axios.post('http://localhost:3085/user/login', {
-        email:payload.email, password:payload.password
-      },{
-        withCredentials : true
-      }).then((data) => { // promise
-        console.log(data);
-      }).catch((error)=>{
-        console.error(error);
+export const actions = {
+  signUp({ commit, state }, payload) {
+    this.$axios.post('http://localhost:3085/user', {
+      email: payload.email,
+      nickname: payload.nickname,
+      password: payload.password,
+    }, {
+      withCredentials: true,
+    })
+      .then((res) => {
+        commit('setMe', res.data);
+      })
+      .catch((err) => {
+        console.error(err);
       });
-      commit('setMe', payload);
-    },
-    logOut({commit}, payload){
-      commit('setMe', null);
+  },
+  logIn({ commit }, payload) {
+    this.$axios.post('http://localhost:3085/user/login', {
+      email: payload.email,
+      password: payload.password,
+    }, {
+      withCredentials: true,
+    })
+      .then((res) => {
+        commit('setMe', res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+  logOut({ commit }) {
+    this.$axios.post('http://localhost:3085/user/logout', {}, {
+      withCredentials: true,
+    })
+      .then((data) => {
+        commit('setMe', null);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
 
-    },
-    changeNickname({commit}, payload){
-      commit('changeNickname', payload)
-    },
-    addFollowing({commit}, payload){
-      commit('addFollowing', payload)
-    },
-    addFollower({commit}, payload){
-      commit('addFollower', payload)
-    },
-    removeFollowing({commit}, payload){ //비동기요청
-      commit('removeFollowing', payload)
-    },
-    removeFollower({commit}, payload){
-      commit('removeFollower', payload)
-    },
-    loadFollowers({ commit, state }, payload) {
-      if (state.hasMoreFollower) {
-        commit('loadFollowers');
-      }
-    },
-    loadFollowings({ commit, state }, payload) {
-      if (state.hasMoreFollowing) {
-        commit('loadFollowings');
-      }
-    },
-}
+  },
+  changeNickname({ commit }, payload) {
+    commit('changeNickname', payload);
+  },
+  addFollowing({ commit }, payload) {
+    commit('addFollowing', payload);
+  },
+  addFollower({ commit }, payload) {
+    commit('addFollower', payload);
+  },
+  removeFollowing({ commit }, payload) {
+    // 비동기 요청
+    commit('removeFollowing', payload);
+  },
+  removeFollower({ commit }, payload) {
+    commit('removeFollower', payload);
+  },
+  loadFollowers({ commit, state }, payload) {
+    if (state.hasMoreFollower) {
+      commit('loadFollowers');
+    }
+  },
+  loadFollowings({ commit, state }, payload) {
+    if (state.hasMoreFollowing) {
+      commit('loadFollowings');
+    }
+  },
+};
