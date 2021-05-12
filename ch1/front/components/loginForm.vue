882ea8b759fa48cc8bec48995926a5162b304e6f
minthing <minjeongandk@gmail.com>
2021-05-12 22:43:58 +0900

    바인딩 수정

--- ch1/front/components/loginForm.vue
+++ ch1/front/components/loginForm.vue
@@ -3,8 +3,9 @@
     <v-card>
         <v-form ref="form" v-model="valid" @submit.prevent="onSubmitForm">
             <v-container>
-                <v-text-field label="email" :rules="emailRules" type="email" required />
-                <v-text-field label="password" :rules="passwordRules" type="password" required />
+              <!-- https://joshua1988.github.io/web-development/vuejs/v-model-usage/ -->
+                <v-text-field v-model="email" label="email" :rules="emailRules" type="email" required />
+                <v-text-field v-model="password" label="password" :rules="passwordRules" type="password" required />
                 <v-btn color="green" type="submit" :disabled="!valid">login</v-btn>
                 <v-btn nuxt to="/signup">sign up</v-btn>
             </v-container>
@@ -20,40 +21,39 @@
 </template>
 <script>
 export default {
-  methods:{
-    onSubmitForm(){ // action은 비동기 이므로 위와 아래의 실행완료 시점이 다를 수 있음
+    data() {
+      return {
+        valid: false,
+        email: '',
+        password: '',
+        emailRules: [
+          v => !!v || '이메일은 필수입니다.',
+          v => /.+@.+/.test(v) || '이메일이 유효하지 않습니다.',
+        ],
+        passwordRules: [
+          v => !!v || '비밀번호는 필수입니다.',
+        ],
+      };
+    },
+    computed: {
+      me() {
+        return this.$store.state.user.me;
+      },
+    },
+    methods: {
+      onSubmitForm() {
         if (this.$refs.form.validate()) {
           this.$store.dispatch('user/logIn', {
-            password: this.password,
             email: this.email,
+            password: this.password,
           });
-      }
+        }
+      },
+      onLogOut() {
+        this.$store.dispatch('user/logOut');
+      },
     },
-    onLogOut(){
-      this.$store.dispatch('user/logOut');
-    }
-  },
-  computed:{ // store 데이터 불러옴
-  me(){
-    return this.$store.state.user.me
-  }
-  },
-  data(){
-    return {
-      valid : false,
-      email:'',
-      password:'',
-      terms:false,
-      emailRules:[
-        v => !!v || '이메일은 필수 값입니다.', // 넣었는지 확인
-        v => /.+@.+/.test(v) || '이메일이 유효하지 않습니다.', //이메일 형식인지 확인하는 정규표현식
-      ],
-      passwordRules:[
-         v => !!v || '비밀번호는 필수 값입니다.',
-      ]
-    }
-  }
-}
+  };
 </script>
 
 <style>
--- ch1/front/store/user.js
+++ ch1/front/store/user.js
@@ -1,35 +1,49 @@
 export const state = () => ({
-  me: null,
-  followerList: [],
-  followingList: [],
-  hasMoreFollower: true,
-  hasMoreFollowing: true,
-});
+  me :null,
+  followingList:[{
+    id:1, nickname:'one'
+  },{
+    id:2, nickname:'two'
+  },{
+    id:3, nickname: 'three'
+  }],
+  followerList:[{
+    id:1, nickname:'one'
+  },{
+    id:2, nickname:'two'
+  },{
+    id:3, nickname: 'three'
+  }],
+  hasMoreFollower:true,
+  hasMoreFollowing:true
+})
 
 const totalFollowers = 8;
 const totalFollowings = 6;
 const limit = 3;
 
-export const mutations = {
-  setMe(state, payload) {
-    state.me = payload;
+
+export const mutations = { // 일반 객체로 만들어야 함
+  setMe(state, payload) { // payload : state를 바꿀 수 있게 해줌
+
+    state.me = payload; // 비동기 작업이 있으면 안됨! ajax 요청도 안됨!
   },
-  changeNickname(state, payload) {
-    state.me.nickname = payload.nickname;
+  changeNickname(state, payload){
+    state.me.nickname = payload.nickname
   },
-  addFollower(state, payload) {
-    state.followerList.push(payload);
+  addFollowing(state, payload){
+    state.following.push(payload)
   },
-  addFollowing(state, payload) {
-    state.followingList.push(payload);
+  addFollower(state, payload){
+    state.followerList.push(payload)
   },
-  removeFollower(state, payload) {
-    const index = state.followerList.findIndex(v => v.id === payload.id);
-    state.followerList.splice(index, 1);
-  },
-  removeFollowing(state, payload) {
+  removeFollowing(state, payload){ 
     const index = state.followingList.findIndex(v => v.id === payload.id);
-    state.followingList.splice(index, 1);
+    state.followingList.splice(index, 1)
+  },
+  removeFollower(state, payload){
+    const index = state.followerList.findIndex(v => v.id === payload.id);
+    state.followerList.splice(index, 1)
   },
   loadFollowings(state) {
     const diff = totalFollowings - state.followingList.length;
@@ -51,7 +65,10 @@ export const mutations = {
   },
 };
 
-export const actions = {
+export const actions = { //비동기적 작업을 위해 actions를 사용함
+  // context object 안에 있는 것들
+  // commit : mutation 함수를 실행
+  // dispatch : actions를 실행
   signUp({ commit, state }, payload) {
     this.$axios.post('http://localhost:3085/user', {
       email: payload.email,
@@ -93,21 +110,20 @@ export const actions = {
       });
 
   },
-  changeNickname({ commit }, payload) {
-    commit('changeNickname', payload);
+  changeNickname({commit}, payload){
+    commit('changeNickname', payload)
   },
-  addFollowing({ commit }, payload) {
-    commit('addFollowing', payload);
+  addFollowing({commit}, payload){
+    commit('addFollowing', payload)
   },
-  addFollower({ commit }, payload) {
-    commit('addFollower', payload);
+  addFollower({commit}, payload){
+    commit('addFollower', payload)
   },
-  removeFollowing({ commit }, payload) {
-    // 비동기 요청
-    commit('removeFollowing', payload);
+  removeFollowing({commit}, payload){ //비동기요청
+    commit('removeFollowing', payload)
   },
-  removeFollower({ commit }, payload) {
-    commit('removeFollower', payload);
+  removeFollower({commit}, payload){
+    commit('removeFollower', payload)
   },
   loadFollowers({ commit, state }, payload) {
     if (state.hasMoreFollower) {
@@ -119,4 +135,4 @@ export const actions = {
       commit('loadFollowings');
     }
   },
-};
+}
